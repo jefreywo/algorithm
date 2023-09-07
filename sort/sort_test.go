7587d5b8f18a96b2fadbbdc089bcf4c07b3c1e3f
wufangjun <wufangjun@aplum.onaliyun.com>
2023-09-07 11:08:13 +0800

    选择排序和冒泡排序

--- sort/sort_test.go
+++ sort/sort_test.go
@@ -30,3 +30,28 @@ func TestBubbleSort(t *testing.T) {
 		})
 	}
 }
+func TestSelectSort(t *testing.T) {
+	type args struct {
+		arrs []int64
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int64
+	}{
+		{
+			name: "test1",
+			args: args{
+				[]int64{1, 2, 4, 5, 7, 6, 8, 3, 9, 0},
+			},
+			want: []int64{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := SelectSort(tt.args.arrs); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("SelectSort() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
